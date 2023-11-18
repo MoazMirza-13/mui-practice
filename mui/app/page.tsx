4765d2c4be2bfcd0d46c44a20ddc029d5d6dd033
Moaz Mirza <moazmirza13@gmail.com>
2023-11-19 00:31:32 +0500

    @emtion/cache & @emotion/server

--- mui/app/page.tsx
+++ mui/app/page.tsx
@@ -1,5 +1,10 @@
-import React from "react";
+import Button from "@mui/material/Button";
 
-export default function page() {
-  return <div>HOME</div>;
+export default function Page() {
+  return (
+    <>
+      Hello, Next.js!
+      <Button variant="contained">Hello world</Button>
+    </>
+  );
 }
