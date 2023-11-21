390bb9f5f4e734012dc50367bc8257ea887ff654
Moaz Mirza <moazmirza13@gmail.com>
2023-11-22 00:40:57 +0500

    initial changes

--- material-ui-nextjs-ts/src/app/page.tsx
+++ material-ui-nextjs-ts/src/app/page.tsx
@@ -1,21 +1,21 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Unstable_Grid2';
-import Drawer from '@mui/material/Drawer';
-import Typography from '@mui/material/Typography';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import Alert from '@mui/material/Alert';
-import AlertTitle from '@mui/material/AlertTitle';
-import MediaCard from '@/components/MediaCard';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Unstable_Grid2";
+import Drawer from "@mui/material/Drawer";
+import Typography from "@mui/material/Typography";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
+import MediaCard from "@/components/MediaCard";
 
 export default function HomePage() {
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
       <div>
         <Alert severity="info" sx={{ mt: 2, mb: 5 }}>
           <AlertTitle>Hello 👋</AlertTitle>
-          This app uses the Next.js App Router and Material UI v5.
+          This is a Next.js application with Material UI (Practice).
         </Alert>
         <Grid container rowSpacing={3} columnSpacing={3}>
           <Grid xs={6}>
@@ -44,15 +44,15 @@ export default function HomePage() {
           </Grid>
         </Grid>
       </div>
-      <Drawer
+      {/* <Drawer
         sx={{
           width: 320,
           flexShrink: 0,
-          '& .MuiDrawer-paper': {
+          "& .MuiDrawer-paper": {
             width: 320,
-            boxSizing: 'border-box',
-            top: ['48px', '56px', '64px'],
-            height: 'auto',
+            boxSizing: "border-box",
+            top: ["48px", "56px", "64px"],
+            height: "auto",
             bottom: 0,
           },
         }}
@@ -66,7 +66,7 @@ export default function HomePage() {
             </Typography>
           </ListItem>
         </List>
-      </Drawer>
+      </Drawer> */}
     </Box>
   );
 }
